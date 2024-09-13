8c3378cc92d6874336203340c1d4807a1f824c93
NzGhozt7 <s63683@ocean.umt.edu.my>
2024-09-13 11:58:27 +0800

    12345

--- src/views/SenaraiPengadu.vue
+++ src/views/SenaraiPengadu.vue
@@ -21,7 +21,7 @@
         <table class="table table-hover table-striped mt-5">
           <thead>
             <tr>
-              <th scope="col">#</th>
+              <th scope="col">No.</th>
               <th scope="col">Nama</th>
               <th scope="col">IC No</th>
               <th scope="col">Email</th>
